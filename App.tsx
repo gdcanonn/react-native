d4f1c6af26e2458ec0983306578272cc3cee4185
GERMAN <german@MacBook-Pro-de-GERMAN.local>
2023-03-31 22:34:33 -0500

    chore(init) Init firt react native project.

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ContadorScreen } from './src/screens/ContadorScreen'
+
+const App = () => {
+  return (
+    // <HolaMundoScreen />
+    <ContadorScreen />
+  )
+}
+
+export default App
---
+++ src/screens/ContadorScreen.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+export const ContadorScreen = () => {
+  const [contador, setContador] = useState(10)
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Contador: {contador}
+      </Text>
+
+      <TouchableOpacity
+        onPress={() => setContador(contador + 1)}
+      >
+        <View style={styles.botonIncrementar}>
+          <Text>+1</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    top: -15
+  },
+  botonIncrementar: {
+    backgroundColor: 'red',
+    borderRadius: 100
+  }
+})
